4d1b070b416b8011f487ff12c0c17618b077f180
Pyselcongor <Florian.lemerle@hotmail.com>
2022-09-27 11:47:18 +0200

    Update setup-logstash-certs.sh

--- Docker-ElasticStack/scripts/setup-logstash-certs.sh
+++ Docker-ElasticStack/scripts/setup-logstash-certs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cp /var/lib/docker/volumes/elastic_certs/_data/logstash/* /home/pysel/docker/logstash/certs/
-cp /var/lib/docker/volumes/elastic_certs/_data/ca/ca.crt /home/pysel/docker/logstash/certs/
+cp /var/lib/docker/volumes/elastic-stack_certs/_data/logstash/* /docker/elastic-stack/logstash/certs/
+cp /var/lib/docker/volumes/elastic-stack_certs/_data/ca/ca.crt /docker/elastic-stack/logstash/certs/
 
-openssl pkcs8 -inform PEM -in /home/pysel/docker/logstash/certs/logstash.key -topk8 -nocrypt -outform PEM -out /home/pysel/docker/logstash/certs/logstash.pkcs8.key
-chmod 644 /home/pysel/docker/logstash/certs/logstash.pkcs8.key
-
+openssl pkcs8 -inform PEM -in /docker/elastic-stack/logstash/certs/logstash.key -topk8 -nocrypt -outform PEM -out /docker/elastic-stack/logstash/certs/logstash.pkcs8.key
+chmod 644 /docker/elastic-stack/logstash/certs/logstash.pkcs8.key
